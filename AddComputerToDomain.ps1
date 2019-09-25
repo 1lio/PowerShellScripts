1494431d48aa480e46b6b383163a3a797c000c4a
SukhovViacheslav <suhov.vyacheslav.dev@gmail.com>
2019-09-25 16:39:37 +0300

    Добавлен скрипт ввода компьютера в домен!

---
+++ AddComputerToDomain.ps1
@@ -0,0 +1,55 @@
+﻿<# 
+ Скрип ввода компьютера в домен.
+ Для выполнением скрипта дай разрешение Set-ExecutionPolicy Unrestricted, в конце можно выполнить эту же команду,
+ с параметром Restricted, чтобы в дальнейшем запретить выполнение PowerShell скриптов
+#>
+
+# Запрос прав админа
+if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")) {
+    Write-Host "Скрипт запущен без прав администратора!" -ForegroundColor Red
+    Write-Host "Запрос прав..." -ForegroundColor Red
+
+    #запуск нового экземпляра данного скрипта, с запросом прав администратора
+    Start-Process powershell $PSCommandPath -verb runAs
+    Break
+}
+
+try {
+    # установка разрешение выполнения PowerShell скриптов
+    Set-ExecutionPolicy Unrestricted
+    Write-Host "Запрос разрешения на выполение скрипта. Успех!" -ForegroundColor Green
+}
+catch {
+
+    Write-Host "Запрос разрешения на выполение скрипта. Неудача!" -ForegroundColor Red
+    Write-Host "Выполните команду: Set-ExecutionPolicy Unrestricted" -ForegroundColor Cyan
+    powershell.exe -NoExit | Out-Null
+}
+
+Write-Host "Данный скрипт вводит компьютер в домен!" -ForegroundColor Cyan
+$next = Read-Host "Продолжить? [Y]/[N]"
+
+if ($next -eq 'y') {
+    Write-Host "Запускаю процедуру присоединения к домену" -ForegroundColor Cyan
+
+    $nameComuter = Read-Host "Введите имя компьютера"
+    $domainName = Read-Host "Введите имя домена"
+
+    Write-Host "Идет подключение к домену..." -ForegroundColor Cyan
+    $creed = $Host.UI.PromptForCredential("Логин", "Введите данные администратора", "", "NetBiosUserName")
+
+    if ($env:COMPUTERNAME -eq $nameComuter) {
+        # Ввод в домен без переименования компьютера
+        Add-Computer  -ComputerName $env:COMPUTERNAME -DomainName $domainName.ToUpper() -Credential $creed -Restart
+    }
+    else {
+        # Ввод в домен с новым именем
+        Add-Computer  -ComputerName $env:COMPUTERNAME -DomainName $domainName.ToUpper()  -NewName $nameComuter -Credential $creed -Restart
+    }
+
+    powershell.exe -NoExit | Out-Null
+}
+else {
+    # заершение скрипта
+    exit
+}
