29cfd8a7cfe5f443f0a654bba40aa6412012c063
SukhovViacheslav <suhov.vyacheslav.dev@gmail.com>
2019-10-03 11:49:16 +0300

    Добавлен скрипт для добавления пользователя в AD

---
+++ FastCreatedStudentAccaunt.ps1
@@ -0,0 +1,41 @@
+﻿# Скрипт для автоматизации создания учетных записей
+# Необходим в случаях когда необхоимо создать большое колличество учетных записей
+
+#Для работы скрипта необходим модуль AD
+Import-Module ActiveDirectory
+
+# Данный скрипт создает 50 учетных записей студент
+
+$domain = "mydomain"
+$domaintRoot = "com"
+$department = "Students"
+
+# Параметры учетных записей
+$username="student"
+$password = "@$$w0rd"
+$countUsers = 1..50
+
+# Включена ли учетная запись
+$isBlockedAccount = $true
+
+# Запрашивать смену пароля при первом подключении
+$isUpdatedPass = $flase
+
+$org = "OU=$department, DC=$domain, DC=$domaintRoot"
+
+foreach ($i in $countUsers){
+   { New-AdUser -Name $username$i -Path $org -Enabled $isBlockedAccount -ChangePasswordAtLogon $isUpdatedPass  `
+    -AccountPassword (ConvertTo-SecureString «$password» -AsPlainText -force) -passThru 
+   }
+}
+
+# Чтобы дать учетным данны одинаковые права необходимо настроить одну учетную запись, а затем использовать ее в качестве шаблона
+# с помощью параметра -Instance Например:
+
+<# $template = Get-AdUser -Identity ″student″
+  foreach ($i in $countUsers){
+   { New-AdUser -Name $username$i -UserPrincipalName $username$i -Path $org -Instance `
+    $template -Enabled $True -ChangePasswordAtLogon $true `
+    -AccountPassword (ConvertTo-SecureString «$password» -AsPlainText -force) -passThru
+   }
+} #>
---
+++ ImportUserListFromCSV.ps1
@@ -0,0 +1,10 @@
+﻿# Данный скрипт предназначен для импорта пользователей из CSV файла
+# В качестве столбцов необходимо указать атрибуты пользователя
+
+#Для работы скрипта необходим модуль AD
+Import-Module ActiveDirectory
+
+$defPass = "P@$$w0rd"
+$csvPath = Import-CSV -Path ″C:\scripts\users.csv″
+$csvPath | New-AdUser  -Path $org -Enabled $True -ChangePasswordAtLogon $true `
+-AccountPassword (ConvertTo-SecureString $defPass -AsPlainText -force) -passThru
