39cbcf58c844005a1697948c8771739f6e4394da
SukhovViacheslav <suhov.vyacheslav.dev@gmail.com>
2019-10-03 11:56:04 +0300

    Обновлено описание

--- ImportUserListFromCSV.ps1
+++ ImportUserListFromCSV.ps1
@@ -1,5 +1,6 @@
 ﻿# Данный скрипт предназначен для импорта пользователей из CSV файла
 # В качестве столбцов необходимо указать атрибуты пользователя
+# Список все атрибутов можно посмотреть здесь https://docs.microsoft.com/en-us/previous-versions/windows/it-pro/windows-server-2008-R2-and-2008/ee617253(v=technet.10)?redirectedfrom=MSDN
 
 #Для работы скрипта необходим модуль AD
 Import-Module ActiveDirectory
