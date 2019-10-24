247618f312308b1f0dccde0fe16829f2325b0020
SukhovViacheslav <suhov.vyacheslav.dev@gmail.com>
2019-10-24 15:21:20 +0300

    Информация о мониторах которую смог выдрать из параметров монитора

---
+++ Monitors.ps1
@@ -0,0 +1,25 @@
+﻿# Информация о мониторах которую смог выдрать из параметров монитора
+# Задумывался как часть PCREPORT.ps1
+function Get-MonitorDetails
+{
+  param
+  (
+    [Object]
+    [Parameter(Mandatory=$true, ValueFromPipeline=$true, HelpMessage="Data to process")]
+    $InputObject
+  )
+  process
+  {
+    $Manufacturer = ($InputObject.ManufacturerName -notmatch 0 | ForEach-Object{[char]$_}) -join ""
+    $MName = ($InputObject.UserFriendlyName -notmatch 0 | ForEach-Object{[char]$_}) -join ""
+    $MSerial = ($InputObject.SerialNumberID -notmatch 0 | ForEach-Object{[char]$_}) -join ""
+
+    return [pscustomobject]@{
+      Manufacturer = $Manufacturer
+      MName        = $MName
+      MSerial      = $MSerial
+    }
+  }
+}
+
+Get-WmiObject WmiMonitorID -Namespace root\wmi | Get-MonitorDetails
