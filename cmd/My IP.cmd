a779ae125909348c4a39c8b0eb79f044a347fe21
ViacheslavSukhov <svo_efremov@mail.ru>
2020-12-16 10:24:26 +0300

    Вывод ip-адресов

--- cmd/My IP.cmd
+++ cmd/My IP.cmd
@@ -1,5 +1,9 @@
-@echo off
+echo off
+
+REM бежим по сетевухам и собираем адреса (ipv4)
 for /f "tokens=2" %%a in ('netsh interface ipv4 show addresses^|find "IP-"') do set LocIP=%%a& Goto extNetsh
 :extNetsh
+
+REM выводим в консоль результат
 echo %LocIP%
 pause
