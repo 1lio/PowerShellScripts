7f2aa176e6773687e2e53e053cd0d280bd6d7548
ViacheslavSukhov <svo_efremov@mail.ru>
2020-12-16 12:38:55 +0300

    Планировка инвентаризации

---
+++ powershell/ReportSchedule.ps1
@@ -0,0 +1,29 @@
+﻿# Задача на инвентаризацию
+
+# Для работы с Ad Необходим импортнуть модуль или в ролях сервира добавить компонент WMI
+import-module activedirectory
+
+# Получить список всех компьютеров в домене и экспортировать в файл
+get-ADcomputer -Filter * | 
+Where-Object {$a=$_.name; $_.DistinguishedName -ne "CN=$a,DC=dc"} |
+Sort-Object name | Select-Object name | Export-csv C:\Invent\AllComputers.csv -NoTypeInformation
+
+# Опросить компы из списка
+import-csv c:\Invent\AllComputers.csv | foreach {
+$a=$_.name
+
+    # Проверяем что компьютер доступен
+    if ((Test-connection $a -count 2 -quiet) -eq "True")
+    {
+
+    # Создание задачи в планировщике на запуск скрипта отчета о окмпьютере
+    # Время запуска
+    $Trigger= New-ScheduledTaskTrigger -At 10:00am -Daily
+    
+    # Запуск от системы
+    $User= "NT AUTHORITY\SYSTEM"
+    # Запустить скрипт лежащего по сетевому пути
+    $Action= New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "\\Share\scripts\PCReport.ps1"
+    Register-ScheduledTask -TaskName "StartupScript_PS" -Trigger $Trigger -User $User -Action $Action -RunLevel Highest –Force
+    }
+}
