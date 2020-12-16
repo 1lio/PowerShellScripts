ed90596a92ca6662a8f97ff2b36af55cefd960e7
ViacheslavSukhov <svo_efremov@mail.ru>
2020-12-16 12:27:43 +0300

    Экспорт компов домена в CSV

---
+++ powershell/ExportAllPc.ps1
@@ -0,0 +1,11 @@
+﻿# Список всех компьютеров
+
+# Для работы с Ad Необходим импортнуть модуль или в ролях сервира добавить компонент WMI
+import-module activedirectory
+
+# Получить список всех компьютеров в домене и экспортировать в файл
+# Сохранить как
+$ExportFile = "C:\Invent\AllComputers.csv"
+
+get-ADcomputer -Filter * | Where-Object {$a=$_.name; $_.DistinguishedName -ne "CN=$a,DC=dc03"} |
+Sort-Object name | Select-Object name | Export-csv $ExportFile -NoTypeInformation
