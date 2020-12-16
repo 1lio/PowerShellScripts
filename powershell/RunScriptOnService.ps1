ca5af120d72bf4cfe26a61c4ad2124c7380ab19b
ViacheslavSukhov <svo_efremov@mail.ru>
2020-12-16 12:28:05 +0300

    Запустить скрипт как службу

---
+++ powershell/RunScriptOnService.ps1
@@ -0,0 +1,19 @@
+﻿#Данный скрипт создает службу Windows и выполняет указанный скрипт
+ 
+# Название службы
+$ServiceName = "BackupAdminFolder"
+ 
+# Путь по которому лежит-PS скрипт
+$ScripPath = "D:\PowerShell\BackupAdmin.ps1"
+
+$NSSMPath = (Get-Command "C:\tools\nssm\win64\nssm.exe").Source
+$PoShPath = (Get-Command powershell).Source
+$args = '-ExecutionPolicy Bypass -NoProfile -File "{0}"' -f $PoShScriptPath
+& $NSSMPath install $NewServiceName $PoShPath $args
+& $NSSMPath status $NewServiceName
+
+#Запускаем службу
+Start-Service $ServiceName
+
+# Проверка что сужба запущена
+Get-Service $ServiceName
