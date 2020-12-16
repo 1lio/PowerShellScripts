6856cc26197f45a113ba9820ea71c6a49e2f1dd1
ViacheslavSukhov <svo_efremov@mail.ru>
2020-12-16 12:35:04 +0300

    Пример внедрения кода на JS

---
+++ powershell/JS-Test.ps1
@@ -0,0 +1,12 @@
+﻿# Пример внедрения кода на JS
+Add-Type @'
+ class Calc {
+   function sum(a,b){
+       return a + b;
+        }
+      }
+'@ -Language JScript
+
+
+$rect = [Calc]::new()
+$rect.sum(2, 2)
