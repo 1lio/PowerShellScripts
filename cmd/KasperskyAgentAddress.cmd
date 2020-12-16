7a2f2af599bf1d9c83ed7f017a19fc33e1766790
ViacheslavSukhov <svo_efremov@mail.ru>
2020-12-16 10:24:04 +0300

    Смена адреса в Kaspersky Agent

--- cmd/KasperskyAgentAddress.cmd
+++ cmd/KasperskyAgentAddress.cmd
@@ -1,5 +1,8 @@
+REM Данный скрипт задает агенту адрес сервера администрирования Kaspersky
 echo off
 
+REM Путь по умолчанию где лежит агент
 cd C:\Program Files (x86)\Kaspersky Lab\NetworkAgent
 
+REM запускаем утилиту https://support.kaspersky.ru/12336  передаем адрес в качестве параметра
 klmover.exe -address kas
